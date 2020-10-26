9594e30d949f4d6907b6837b3d9668b817065fcc
Francisco Araújo <quico.calado.araujo@gmail.com>
2020-10-26 17:56:37 +0000

    Added preloader :)

--- loading.js
+++ loading.js
@@ -1,17 +1,18 @@
-function onReady(callback) {
-  var intervalId = window.setInterval(function() {
-    if (document.getElementsByTagName('body')[0] !== undefined) {
-      window.clearInterval(intervalId);
-      callback.call(this);
-    }
-  }, 5000);
-}
+$(document).ready(function() {
+  setTimeout(function() {
+    $('.wrapper').addClass('loaded');
+    
+  }, 3000);
+});
 
-function setVisible(selector, visible) {
-  document.querySelector(selector).style.display = visible ? 'block' : 'none';
-}
 
-onReady(function() {
-  setVisible('.page', true);
-  setVisible('#loading', false);
+
+jQuery(function(){
+
+  $(window).load(function(){
+  
+  $('.wrapper').removeClass('preload');
+  
+  });
+
 });
