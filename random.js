dd5fde1a26f6d36524f61717ee1a4e2a093748ee
Francisco Araújo <quico.calado.araujo@gmail.com>
2020-10-27 23:57:52 +0000

    updated random Gallery

--- random.js
+++ random.js
@@ -1,41 +1,4 @@
 const numImagesAvailable = 48;
-const imageWidth = 480; //your desired image width in pixels
-const imageHeight = 480; //desired image height in pixels
-
-
-function getGalleryItem(){
-    let items = [];
-    let j = 0;
-    for(let i=1;i<numImagesAvailable+1;i++){
-      items[j] = "/images/foto" + i + ".jpg";
-      j++;
-      items[j] = "/images/foto" + i + "-small.jpg";
-      j++;
-    }
-    return items
-}
-
-function renderGalleryItem(){
-
-  let items = [];
-  var ranNums = [];
-
-  for(let i=0;i<numImagesAvailable;i++){
-    ranNums[i] = i;
-  }
-  
-  ranNums = randOrder(ranNums);
-  console.log(ranNums);
-  items = getGalleryItem();
-
-  for(let i=0;i<numImagesAvailable;i++){ 
-    var imagem = document.createElement("IMG");
-    imagem.setAttribute("class", "image");
-    imagem.setAttribute("src", items[ranNums[i]]);
-    imagem.setAttribute("data-src", items[ranNums[i]]);
-    console.log(document.getElementsByClassName("column-s")[0]);
-  }
-}
 
 function randOrder(array){
   var i = array.length,
@@ -54,4 +17,65 @@ function randOrder(array){
   return array;
 }
 
-renderGalleryItem();
+window.onload = function renderGalleryItem(){
+
+  let items = [],
+      itemsSmall = [];
+  var ranNums = [];
+
+  for(let i=0;i<numImagesAvailable;i++){
+    ranNums[i] = i;
+  }
+
+  for(let i=1;i<numImagesAvailable+1;i++){
+    items[i-1] = "images/foto" + i + ".jpg";
+  }
+
+  for(let i=1;i<numImagesAvailable+1;i++){
+    itemsSmall[i-1] = "images/foto" + i + "-small.jpg";
+  }
+
+  
+  ranNums = randOrder(ranNums);
+
+  for(let i=0;i<numImagesAvailable;i++){ 
+    var imagem = document.createElement("IMG");
+    imagem.setAttribute("class", "image");
+    imagem.setAttribute("src", itemsSmall[ranNums[i]]);
+    imagem.setAttribute("data-src", items[ranNums[i]]);
+
+    var link = document.createElement("A");
+    link.setAttribute("href", items[ranNums[i]])
+    link.appendChild(imagem);
+
+    var div = document.createElement("DIV");
+    div.setAttribute("class", "lista");
+    div.appendChild(link)
+
+    let col1 = document.getElementsByClassName("column-s")[0].offsetHeight;
+    let col2 = document.getElementsByClassName("column-s")[1].offsetHeight;
+    let col3 = document.getElementsByClassName("column-s")[2].offsetHeight;
+    let col4 = document.getElementsByClassName("column-s")[3].offsetHeight;
+    
+
+    switch(Math.min(col1, col2, col3, col4)){
+      case col1:
+        document.getElementsByClassName("column-s")[0].appendChild(div);
+        break;
+      case col2:
+        document.getElementsByClassName("column-s")[1].appendChild(div);
+        break;
+      case col3:
+        document.getElementsByClassName("column-s")[2].appendChild(div);
+        break;
+      case col4:
+        document.getElementsByClassName("column-s")[3].appendChild(div);
+        break;
+      default:
+        window.alert("Erro! :(");
+    }
+
+    console.log(col1, col2, col3, col4);
+  }
+}
+
